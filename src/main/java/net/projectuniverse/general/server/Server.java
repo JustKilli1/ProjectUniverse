32d2101d8079f3fcc87aedaae8b202b617085d34
JustKilli <justkilli@outlook.de>
2023-03-20 20:31:23 +0100

    Base Minestom Server implementation

--- src/main/java/net/projectuniverse/general/server/Server.java
+++ src/main/java/net/projectuniverse/general/server/Server.java
@@ -1,6 +1,13 @@
 package net.projectuniverse.general.server;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
 import net.projectuniverse.general.terminal.ServerTerminal;
 
 public class Server {
@@ -12,11 +19,26 @@ public class Server {
      * Starts the Server
      * */
     public static void start() {
-        MinecraftServer.setTerminalEnabled(false);
         server = MinecraftServer.init();
+        MinecraftServer.setTerminalEnabled(false);
         terminal = new ServerTerminal();
         terminal.start();
-        server.start("127.0.0.1", 25565);
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator(unit ->
+                unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        server.start("5.75.144.98", 25565);
     }
 
     /**
