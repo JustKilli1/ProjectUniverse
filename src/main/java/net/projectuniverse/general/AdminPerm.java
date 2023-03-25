93303ce5f689388541a1e54a29da6aba19e0e851
JustKilli <justkilli@outlook.de>
2023-03-25 01:46:35 +0100

    UNI-44 Admin Permission added

---
+++ src/main/java/net/projectuniverse/general/AdminPerm.java
@@ -0,0 +1,25 @@
+package net.projectuniverse.general;
+
+import net.minestom.server.entity.Player;
+import net.minestom.server.permission.Permission;
+
+public enum AdminPerm {
+
+    IGNORE_CHAT_CLEAR("chat.ignore_chat_clear")
+
+    ;
+    private static final String prefix = "projectuniverse.admin.";
+    private String perm;
+
+    AdminPerm(String perm) {
+        this.perm = perm;
+    }
+
+    public static boolean hasPerm(Player target, AdminPerm perm) {
+        return target.hasPermission(new Permission(perm.getPerm()));
+    }
+
+    public String getPerm() {
+        return prefix + perm;
+    }
+}
