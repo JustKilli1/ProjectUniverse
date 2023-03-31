5bd02268a0b17a393fd5cc34c7bb498310102494
JustKilli <justkilli@outlook.de>
2023-04-01 01:48:17 +0200

    sends no perm message

--- src/main/java/net/projectuniverse/general/AdminPerm.java
+++ src/main/java/net/projectuniverse/general/AdminPerm.java
@@ -2,6 +2,9 @@ package net.projectuniverse.general;
 
 import net.minestom.server.entity.Player;
 import net.minestom.server.permission.Permission;
+import net.projectuniverse.general.config.configs.PlayerMessagesConfig;
+import net.projectuniverse.general.messenger.MessageDesign;
+import net.projectuniverse.general.messenger.Messenger;
 
 public enum AdminPerm {
 
@@ -19,7 +22,9 @@ public enum AdminPerm {
     }
 
     public static boolean has(Player target, AdminPerm perm) {
-        return target.hasPermission(new Permission(perm.getPerm()));
+        boolean hasPerm = target.hasPermission(new Permission(perm.getPerm()));
+        if(!hasPerm) Messenger.sendMessage(target, MessageDesign.PLAYER_MESSAGE, PlayerMessagesConfig.NO_PERMISSION.getValue());
+        return hasPerm;
     }
 
     public String getPerm() {
