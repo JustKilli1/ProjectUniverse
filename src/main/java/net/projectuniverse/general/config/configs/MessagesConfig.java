17935fc95bd0ad342196682cb221cd845a3fe593
JustKilli <justkilli@outlook.de>
2023-04-01 17:49:31 +0200

    Param name used in Message

--- src/main/java/net/projectuniverse/general/config/configs/MessagesConfig.java
+++ src/main/java/net/projectuniverse/general/config/configs/MessagesConfig.java
@@ -6,15 +6,16 @@ import net.projectuniverse.general.config.ConfigValue;
 public class MessagesConfig extends ConfigManager {
 
     public static final ConfigValue NO_PERMISSION = new ConfigValue("generell.no_permission", "You don't have Permission to use this Command");
+    public static final ConfigValue PLAYER_JOINED = new ConfigValue("generell.player_join_message", "Player " + MessagesParams.PLAYER.getName() + " joined");
     public static final ConfigValue CLEAR_CHAT_DONE = new ConfigValue("chat.clear_chat.done", "Chat has been cleared.");
-    public static final ConfigValue MUTE_CHAT_CHANGED = new ConfigValue("chat.mute_chat", "Global mute changed to #muted#", MessagesParams.MUTED);
+    public static final ConfigValue MUTE_CHAT_CHANGED = new ConfigValue("chat.mute_chat", "Global mute changed to " + MessagesParams.MUTED.getName(), MessagesParams.MUTED);
     public static final ConfigValue TEAM_CHAT_ACTIVATED = new ConfigValue("chat.team_chat.activated", "Team Chat activated");
     public static final ConfigValue TEAM_CHAT_DEACTIVATED = new ConfigValue("chat.team_chat.deactivated", "Team Chat deactivated");
     public static final ConfigValue CHAT_MUTED = new ConfigValue("chat.chat_muted", "The chat is muted at the moment.");
-    public static final ConfigValue KICK_PLAYER_NOT_FOUND = new ConfigValue("punishment_system.kick.player_not_found", "Could not find a Player with the Name #player#", MessagesParams.PLAYER);
+    public static final ConfigValue KICK_PLAYER_NOT_FOUND = new ConfigValue("punishment_system.kick.player_not_found", "Could not find a Player with the Name " + MessagesParams.PLAYER.getName(), MessagesParams.PLAYER);
     public static final ConfigValue KICK_YOURSELF = new ConfigValue("punishment_system.kick.kick_yourself", "You cannot kick yourself");
     public static final ConfigValue CANT_KICK_PLAYER = new ConfigValue("punishment_system.kick.cannot_kick_player", "You don't have Permission to kick this Player");
-    public static final ConfigValue KICK_SUCCESS = new ConfigValue("punishment_system.kick.kick_success", "Player #player# kicked for \"#reason#\"", MessagesParams.PLAYER, MessagesParams.PUNISHMENT_REASON);
+    public static final ConfigValue KICK_SUCCESS = new ConfigValue("punishment_system.kick.kick_success", "Player " + MessagesParams.PLAYER.getName() + " kicked for " + MessagesParams.PUNISHMENT_REASON.getName(), MessagesParams.PLAYER, MessagesParams.PUNISHMENT_REASON);
     private static final MessagesConfig instance = new MessagesConfig();
 
 
