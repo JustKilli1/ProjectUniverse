0de101a50fa1e28327bab4c5e80d20854956d721
JustKilli <justkilli@outlook.de>
2023-04-01 15:50:13 +0200

    UNI-51 Logger Builder added

---
+++ src/main/java/net/projectuniverse/general/logging/loggers/LoggerBuilder.java
@@ -0,0 +1,30 @@
+package net.projectuniverse.general.logging.loggers;
+
+import net.projectuniverse.general.logging.ILogger;
+import net.projectuniverse.general.logging.type.IOutputPrinter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoggerBuilder {
+
+    private String loggerName;
+    private List<IOutputPrinter> printer;
+
+    public LoggerBuilder(String loggerName) {
+        this.loggerName = loggerName;
+        printer = new ArrayList<>();
+    }
+
+    public LoggerBuilder addOutputPrinter(IOutputPrinter outputPrinter) {
+        printer.add(outputPrinter);
+        return this;
+    }
+
+    public ILogger build() {
+        return new BaseLogger(loggerName, printer);
+    }
+
+
+
+}
