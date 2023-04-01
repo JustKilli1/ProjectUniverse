5ce18d657be027244fdca3fbcb2a68c3633136ca
JustKilli <justkilli@outlook.de>
2023-04-01 18:38:04 +0200

    UNI-54 Game Mode Command added

--- src/main/java/net/projectuniverse/general/config/configs/MessagesConfig.java
+++ src/main/java/net/projectuniverse/general/config/configs/MessagesConfig.java
@@ -7,6 +7,8 @@ public class MessagesConfig extends ConfigManager {
 
     public static final ConfigValue NO_PERMISSION = new ConfigValue("generell.no_permission", "You don't have Permission to use this Command");
     public static final ConfigValue PLAYER_JOINED = new ConfigValue("generell.player_join_message", "Player " + MessagesParams.PLAYER.getName() + " joined", MessagesParams.PLAYER);
+    public static final ConfigValue GAME_MODE_CHANGED = new ConfigValue("generell.game_mode.changed", "GameMode changed to " + MessagesParams.GAME_MODE.getName() + " joined", MessagesParams.GAME_MODE);
+    public static final ConfigValue GAME_MODE_NOT_FOUND = new ConfigValue("generell.game_mode.not_found", "Could not find Gamemode with the id " + MessagesParams.GAME_MODE.getName(), MessagesParams.GAME_MODE);
     public static final ConfigValue CLEAR_CHAT_DONE = new ConfigValue("chat.clear_chat.done", "Chat has been cleared.");
     public static final ConfigValue MUTE_CHAT_CHANGED = new ConfigValue("chat.mute_chat", "Global mute changed to " + MessagesParams.MUTED.getName(), MessagesParams.MUTED);
     public static final ConfigValue TEAM_CHAT_ACTIVATED = new ConfigValue("chat.team_chat.activated", "Team Chat activated");
--- src/main/java/net/projectuniverse/general/config/configs/MessagesParams.java
+++ src/main/java/net/projectuniverse/general/config/configs/MessagesParams.java
@@ -7,5 +7,6 @@ public class MessagesParams {
     public static final ConfigParam MUTED = new ConfigParam("muted", "Muted");
     public static final ConfigParam PLAYER = new ConfigParam("player", "Player");
     public static final ConfigParam PUNISHMENT_REASON = new ConfigParam("reason", "Reason");
+    public static final ConfigParam GAME_MODE = new ConfigParam("gamemode", "Game Mode");
 
 }
