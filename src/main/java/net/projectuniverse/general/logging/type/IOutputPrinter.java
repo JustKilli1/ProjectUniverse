93910445f4fee652cce7800cd661910dbdf292a3
JustKilli <justkilli@outlook.de>
2023-04-01 14:52:43 +0200

    UNI-51 format Params changed, Comments added

--- src/main/java/net/projectuniverse/general/logging/type/IOutputPrinter.java
+++ src/main/java/net/projectuniverse/general/logging/type/IOutputPrinter.java
@@ -6,9 +6,39 @@ import java.util.List;
 
 public interface IOutputPrinter {
 
+    /**
+     * Writes A LogMessage to the designated Output Target
+     * @param level The {@link LogLevel} of the Log
+     * @param message The Message that gets written to the designated Output Target
+     * */
     void print(LogLevel level, List<String> message);
+    /**
+     * Writes A LogMessage to the designated Output Target
+     * @param level The {@link LogLevel} of the Log
+     * @param message The Message that gets written to the designated Output Target
+     * */
     void print(LogLevel level, String message);
-    String format(List<String> message);
-    String format(String message);
+
+    /**
+     * Method for Logger to Format a File Log Message
+     * @param logLevel LogLevel
+     * @param loggerName Name of the calling logger
+     * @param message Custom Message
+     * @param ex occurring Exception
+     * @return Formatted LogMessage
+     * {@link LogLevel}
+     * */
+    String format(LogLevel logLevel, String loggerName, List<String> message, Exception ex);
+
+    /**
+     * Method for Logger to Format a File Log Message
+     * @param logLevel LogLevel
+     * @param loggerName Name of the calling logger
+     * @param message Custom Message
+     * @param ex occurring Exception
+     * @return Formatted LogMessage
+     * {@link LogLevel}
+     * */
+    String format(LogLevel logLevel, String loggerName, String message, Exception ex);
 
 }
