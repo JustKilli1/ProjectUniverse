aa4d5c80f0410df84535e4f1efc208210f96e0ff
JustKilli <justkilli@outlook.de>
2023-04-03 22:23:19 +0200

    UNI-71 arena can start game

---
+++ src/main/java/net/projectuniverse/general/arena_system/Arena.java
@@ -0,0 +1,30 @@
+package net.projectuniverse.general.arena_system;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.projectuniverse.general.instance.InstanceHandler;
+
+public class Arena {
+
+    private static InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+    private InstanceContainer instance;
+    private Player player;
+
+    public Arena(Player player) {
+        this.player = player;
+        init();
+    }
+
+    private void init() {
+        instance = InstanceHandler.TOWER_DEFENCE_ARENA.copy();
+        instanceManager.registerInstance(instance);
+    }
+
+    public void start() {
+        player.setInstance(instance, new Pos(4, 71, 19));
+    }
+
+}
