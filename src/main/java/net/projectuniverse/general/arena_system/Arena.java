9f5470b79a6ed2559959391336ea395f0a683181
JustKilli <justkilli@outlook.de>
2023-04-03 23:18:46 +0200

    added getter for instance

--- src/main/java/net/projectuniverse/general/arena_system/Arena.java
+++ src/main/java/net/projectuniverse/general/arena_system/Arena.java
@@ -1,15 +1,12 @@
 package net.projectuniverse.general.arena_system;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.InstanceContainer;
-import net.minestom.server.instance.InstanceManager;
 import net.projectuniverse.general.instance.InstanceHandler;
 
 public class Arena {
 
-    private static InstanceManager instanceManager = MinecraftServer.getInstanceManager();
     private InstanceContainer instance;
     private Player player;
 
@@ -20,7 +17,6 @@ public class Arena {
 
     private void init() {
         instance = InstanceHandler.TOWER_DEFENCE_ARENA.copy();
-        instanceManager.registerInstance(instance);
     }
 
     public void start() {
@@ -29,7 +25,9 @@ public class Arena {
 
     public void stop() {
         player.setInstance(InstanceHandler.LOBBY, new Pos(0, 100, 0));
-        instanceManager.unregisterInstance(instance);
     }
 
+    public InstanceContainer getInstance() {
+        return instance;
+    }
 }
