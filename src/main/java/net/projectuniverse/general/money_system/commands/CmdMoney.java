0dcddbc78cff93b485465fc8615c02e6d5e1ed80
JustKilli <justkilli@outlook.de>
2023-06-06 17:16:26 +0200

    UNI-79 money command added

---
+++ src/main/java/net/projectuniverse/general/money_system/commands/CmdMoney.java
@@ -0,0 +1,45 @@
+package net.projectuniverse.general.money_system.commands;
+
+import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity;
+import net.minestom.server.entity.Player;
+import net.minestom.server.utils.entity.EntityFinder;
+import net.projectuniverse.general.commands.UniverseCommand;
+import net.projectuniverse.general.commands.command_executor.DefaultExecutor;
+import net.projectuniverse.general.config.configs.MessagesConfig;
+import net.projectuniverse.general.config.configs.MessagesParams;
+import net.projectuniverse.general.messenger.MessageDesign;
+import net.projectuniverse.general.messenger.Messenger;
+import net.projectuniverse.general.money_system.PlayerPurse;
+import net.projectuniverse.general.money_system.database.DBALMoney;
+import net.projectuniverse.general.money_system.database.DBHMoney;
+
+public class CmdMoney extends UniverseCommand {
+
+
+    private final DBALMoney sql;
+    private final DBHMoney dbHandler;
+
+    public CmdMoney(DBALMoney sql, DBHMoney dbHandler) {
+        super("money", "Shows how much money a Player has", "money (Player-Name)");
+        this.sql = sql;
+        this.dbHandler = dbHandler;
+
+        setDefaultExecutor(new DefaultExecutor(getUsage()));
+
+        addSyntax((sender, context) -> {
+            if(!(sender instanceof Player senderPlayer)) return;
+
+            PlayerPurse.Currency currency = PlayerPurse.Currency.UNIS;
+            PlayerPurse playerPurse = dbHandler.getPlayerPurse(senderPlayer, currency).orElse(new PlayerPurse(senderPlayer, PlayerPurse.Currency.UNIS, sql));
+
+            Messenger.sendMessage(sender, MessageDesign.SERVER_MESSAGE, MessagesConfig.SHOW_PLAYER_MONEY.clone()
+                    .setConfigParamValue(MessagesParams.AMOUNT.clone().setValue(String.valueOf(playerPurse.getAmount())))
+                    .setConfigParamValue(MessagesParams.CURRENCY.clone().setValue(currency.getDisplayName() + (playerPurse.getAmount() > 1 ? "s" : "")))
+                    .setConfigParamValue(MessagesParams.CURRENCY_SYMBOL.clone().setValue(currency.getSymbol()))
+                    .getValue());
+
+        });
+
+    }
+
+}
