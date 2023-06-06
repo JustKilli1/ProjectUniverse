092cd4798d790872a8f2fcac93eaba40b3469b14
JustKilli <justkilli@outlook.de>
2023-06-06 16:12:53 +0200

    Minestom Version changed

--- src/main/java/net/projectuniverse/base/Main.java
+++ src/main/java/net/projectuniverse/base/Main.java
@@ -1,10 +1,9 @@
 package net.projectuniverse.base;
 
+import net.minestom.server.MinecraftServer;
 import net.projectuniverse.general.server.Server;
 
 public class Main {
-
-
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
