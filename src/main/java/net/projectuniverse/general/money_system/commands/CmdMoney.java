973874dd900adcbd7055c5dfd17651a4e58d0b91
JustKilli <justkilli@outlook.de>
2023-06-06 17:17:16 +0200

    UNI-86 Show Other Player money

--- src/main/java/net/projectuniverse/general/money_system/commands/CmdMoney.java
+++ src/main/java/net/projectuniverse/general/money_system/commands/CmdMoney.java
@@ -26,6 +26,28 @@ public class CmdMoney extends UniverseCommand {
 
         setDefaultExecutor(new DefaultExecutor(getUsage()));
 
+        ArgumentEntity playerArg = PLAYER_ARGUMENT;
+
+        addSyntax((sender, context) -> {
+            EntityFinder playerFinder = context.get(playerArg);
+            if(!(sender instanceof Player senderPlayer)) return;
+            Player targetPlayer = playerFinder.findFirstPlayer(sender);
+            if(targetPlayer == null) {
+                Messenger.sendMessage(sender, MessageDesign.SERVER_MESSAGE, MessagesConfig.PLAYER_NOT_FOUND.clone()
+                        .setConfigParamValue(MessagesParams.PLAYER.clone().setValue(playerArg.toString()))
+                        .getValue());
+                return;
+            }
+            PlayerPurse.Currency currency = PlayerPurse.Currency.UNIS;
+            PlayerPurse targetPurse = dbHandler.getPlayerPurse(targetPlayer, currency).orElse(new PlayerPurse(targetPlayer, PlayerPurse.Currency.UNIS, sql));
+            Messenger.sendMessage(sender, MessageDesign.SERVER_MESSAGE, MessagesConfig.SHOW_PLAYER_MONEY_TARGET.clone()
+                    .setConfigParamValue(MessagesParams.AMOUNT.clone().setValue(String.valueOf(targetPurse.getAmount())))
+                    .setConfigParamValue(MessagesParams.PLAYER.clone().setValue(senderPlayer.getUsername()))
+                    .setConfigParamValue(MessagesParams.CURRENCY_SYMBOL.clone().setValue(currency.getSymbol()))
+                    .getValue());
+
+        }, playerArg);
+
         addSyntax((sender, context) -> {
             if(!(sender instanceof Player senderPlayer)) return;
 
