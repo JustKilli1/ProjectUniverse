3c97100784ab360ecf4f8f4ccfdcfff529b1902e
JustKilli <justkilli@outlook.de>
2023-07-07 06:55:45 +0200

    loadModules Method improved

--- src/main/java/net/projectuniverse/general/ModuleLoader.java
+++ src/main/java/net/projectuniverse/general/ModuleLoader.java
@@ -9,7 +9,9 @@ import org.reflections.Reflections;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModuleLoader {
 
@@ -43,18 +45,27 @@ public class ModuleLoader {
      * @return a List of Module objects representing the loaded modules
      */
     public List<Module> loadModules() {
-        List<Module> modules = new ArrayList<>();
         Reflections reflections = new Reflections("net.projectuniverse");
         Set<Class<? extends Module>> classes = reflections.getSubTypesOf(Module.class);
-        classes.forEach(clazz -> {
-            try {
-                Module module = clazz.getConstructor().newInstance();
-                modules.add(module);
-            } catch (Exception ex) {
-                logger.log(LogLevel.ERROR, "Failed to create module instance for module " + clazz.getName() + " with error: ", ex);
-            }
-        });
-        return modules;
+        return classes.stream()
+                .map(this::createModule)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Creates a module instance of the given class.
+     *
+     * @param clazz the class of the module
+     * @return an instance of the module or null if any exception occurred
+     */
+    private Module createModule(Class<? extends Module> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (Exception ex) {
+            logger.log(LogLevel.ERROR, "Failed to create module instance for module " + clazz.getName() + " with error: ", ex);
+            return null;
+        }
     }
 
     /**
@@ -70,7 +81,9 @@ public class ModuleLoader {
     }
 
     public void stopModules() {
+        logger.log(LogLevel.INFO, "Module Shutdown...");
         modules.forEach(Module::stop);
+        logger.log(LogLevel.INFO, "Module Shutdown successfully.");
     }
 
     public void reloadModules() {
