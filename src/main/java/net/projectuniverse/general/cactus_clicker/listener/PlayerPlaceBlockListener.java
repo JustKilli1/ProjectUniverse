b3b681cc430ac8706d2c8f2267a45ccbb9399588
JustKilli <justkilli@outlook.de>
2023-07-28 05:15:06 +0200

    refactored to improve readability

--- src/main/java/net/projectuniverse/general/cactus_clicker/listener/PlayerPlaceBlockListener.java
+++ src/main/java/net/projectuniverse/general/cactus_clicker/listener/PlayerPlaceBlockListener.java
@@ -1,14 +1,18 @@
 package net.projectuniverse.general.cactus_clicker.listener;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Player;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerPlaceBlockListener {
+
+    public static final List<Block> PLACEABLE_BLOCKS = List.of(Block.CACTUS, Block.SAND, Block.RED_SAND);
 
     public PlayerPlaceBlockListener() {
         onBlockPlace();
@@ -20,23 +24,36 @@ public class PlayerPlaceBlockListener {
             Instance instance = event.getInstance();
             Block targetBlock = event.getBlock();
             if(targetBlock.equals(Block.CACTUS)) handleCactusPlace(instance, event);
+            else handleBlockPlace(instance, event);
         });
     }
 
     private void handleCactusPlace(Instance instance, PlayerBlockPlaceEvent event) {
-        //check if block is placed on sand or cactus
-        Block placedOn = instance.getBlock(event.getBlockPosition().add(0, -1, 0));
-        if(placedOn != Block.SAND && placedOn != Block.RED_SAND && placedOn != Block.CACTUS) {
-            event.setCancelled(true);
-            return;
-        }
+        Block baseBlock = instance.getBlock(event.getBlockPosition().add(0, -1, 0));
+        if (!validBaseBlock(baseBlock) || hasNonAirSurroundingBlocks(instance, event)) event.setCancelled(true);
+    }
+    private boolean validBaseBlock(Block baseBlock) {
+        return PLACEABLE_BLOCKS.stream().anyMatch(block -> block.equals(baseBlock));
+    }
 
-        //Check if surrounding blocks are air
-        Block blockAbove = instance.getBlock(event.getBlockPosition().add(0, 1, 0));Block blockNorth = instance.getBlock(event.getBlockPosition().add(0, 0, 1));
-        Block blockEast = instance.getBlock(event.getBlockPosition().add(1, 0, 0));
-        Block blockSouth = instance.getBlock(event.getBlockPosition().add(0, 0, -1));
-        Block blockWest = instance.getBlock(event.getBlockPosition().add(-1, 0, 0));
-        if(blockAbove != Block.AIR || blockNorth != Block.AIR || blockEast != Block.AIR || blockSouth != Block.AIR || blockWest != Block.AIR) event.setCancelled(true);
+    private boolean hasNonAirSurroundingBlocks(Instance instance, PlayerBlockPlaceEvent event) {
+        List<Block> surroundingBlocks = getSurroundingBlocks(instance, event.getBlockPosition());
+        return surroundingBlocks.stream().anyMatch(block -> block != Block.AIR);
+    }
+
+    private void handleBlockPlace(Instance instance, PlayerBlockPlaceEvent event) {
+        List<Block> surroundingBlocks = getSurroundingBlocks(instance, event.getBlockPosition());
+        if (surroundingBlocks.stream().anyMatch(block -> block.equals(Block.CACTUS))) event.setCancelled(true);
+    }
+
+    private List<Block> getSurroundingBlocks(Instance instance, Point pos) {
+        return List.of(
+                instance.getBlock(pos.add(0, 1, 0)),
+                instance.getBlock(pos.add(0, 0, 1)),
+                instance.getBlock(pos.add(1, 0, 0)),
+                instance.getBlock(pos.add(0, 0, -1)),
+                instance.getBlock(pos.add(-1, 0, 0))
+    );
     }
 
 }
