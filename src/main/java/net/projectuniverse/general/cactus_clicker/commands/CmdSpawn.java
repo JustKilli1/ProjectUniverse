a9e4a52208fa674ba2291d2d006c8e3e78fff3f7
JustKilli <justkilli@outlook.de>
2023-07-28 04:05:49 +0200

    spawn command added

---
+++ src/main/java/net/projectuniverse/general/cactus_clicker/commands/CmdSpawn.java
@@ -0,0 +1,28 @@
+package net.projectuniverse.general.cactus_clicker.commands;
+
+import net.minestom.server.entity.Player;
+import net.projectuniverse.general.cactus_clicker.instance_management.InstanceManagement;
+import net.projectuniverse.general.commands.UniverseCommand;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CmdSpawn extends UniverseCommand {
+    /**
+     * Creates a new UniverseCommand with the specified name, description, usage, and aliases.
+     *
+     * @param name        the name of the command (cannot be null)
+     * @param description the description of the command
+     * @param usage       the usage instructions for the command (prefixed with "/")
+     * @param aliases     optional aliases for the command
+     */
+    public CmdSpawn() {
+        super("spawn", "Teleports you to the Spawn", "spawn");
+
+        addSyntax((sender, context) -> {
+           if(!(sender instanceof Player player)) return;
+           if(!InstanceManagement.hasInstance(player)) return;
+           InstanceManagement.savePlayerInstance(player);
+        });
+
+    }
+}
